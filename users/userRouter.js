ef1c251ff664e23b57ed0518c9cd1926b61191a3
John Afolabi <johnafolabi.da@gmail.com>
2020-01-15 13:51:27 +0100

    Add validateUserId middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,39 +1,47 @@
-const express = require('express');
-
+const express = require("express");
+const { getById } = require("./userDb");
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validateUserId, (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  const user = await getById(id);
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(400).json({ message: "invalid user id" });
+  }
 }
 
 function validateUser(req, res, next) {
