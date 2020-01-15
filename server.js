56575c9b92ddc7816a1c58b8830da8dbffbf59e2
John Afolabi <johnafolabi.da@gmail.com>
2020-01-15 15:50:29 +0100

    Implement and test user endpoints

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
+server.use(express.json())
 server.use(logger);
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const { getById } = require("./userDb");
+const {
+  get,
+  getById,
+  getUserPosts,
+  insert,
+  update,
+  remove
+} = require("./userDb");
 const router = express.Router();
 
 router.post("/", validateUser, (req, res) => {
   // do your magic!
+  const newUser = req.body;
+  insert(newUser)
+    .then(createdUser => {
+      res.status(201).json(createdUser);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "The new user could not be added" });
+    });
 });
 
 router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
@@ -12,22 +27,60 @@ router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
 
 router.get("/", (req, res) => {
   // do your magic!
+  get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ errorMessage: "Users cannot be retrieved at this moment" });
+    });
 });
 
 router.get("/:id", validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user);
 });
 
 router.get("/:id/posts", validateUserId, (req, res) => {
   // do your magic!
+  getUserPosts(req.user.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: `${req.user.name} posts cannot be retrieved at this moment`
+      });
+    });
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
   // do your magic!
+  remove(req.user.id)
+    .then(data => {
+      res.status(200).json({ message: `${req.user.name} has been deleted` });
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: `${req.user.name} cannot be deleted at this moment`
+      });
+    });
 });
 
-router.put("/:id", validateUserId, (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res) => {
   // do your magic!
+  update(req.user.id, req.body)
+    .then(async data => {
+      const updatedUser = await getById(req.user.id);
+      res.status(200).json(updatedUser);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: `${req.user.name} cannot be edited at this moment`
+      });
+    });
 });
 
 //custom middleware
